b769f3a1e3cf51b74c0552da16eaae2dc8b0c039
Dario Rivera Haro <dariorivera11@gmail.com>
2026-02-11 20:51:29 -0600

    feat: Add Electron preload script exposing IPC communication methods via contextBridge.

--- editor-texto/electron/preload.cjs
+++ editor-texto/electron/preload.cjs
@@ -1,14 +1,11 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("electronAPI", {
-  // Para escuchar cuando el menú nativo abre algo
   onFileOpened: (callback) =>
     ipcRenderer.on("file-opened", (_event, value) => callback(value)),
 
-  // Para escuchar cuando el menú pide guardar
   onRequestSave: (callback) => ipcRenderer.on("request-save", () => callback()),
 
-  // Para enviar el texto de vuelta al proceso principal y guardarlo
   sendContentToSave: (content) =>
     ipcRenderer.send("send-content-to-save", content),
 });
